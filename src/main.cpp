af1fa8dcc62801c62122a0c53481014579e0d229
Mansen <husenalasady21@gmail.com>
2023-12-20 19:27:37 +0300

    Fix command line display
    Also tweaks the ambient color

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@
 void writeImage(color* pixelBuffer);
 static std::vector<hittable*> scene;
 static int bounceDepth = 0;
-static int samplesPerPixel = 125;
+static int samplesPerPixel = 65;
 color trace (const ray &r)
 {
-    if (bounceDepth >= 250)
+    if (bounceDepth >= 150)
     {
         bounceDepth = 0;
         return color(0, 0, 0);
@@ -37,44 +37,45 @@ color trace (const ray &r)
         {
             if (intersectionInfo.matPtr->emissive)
             {
+                bounceDepth = 0;
                 return attenuation;
             }
             bounceDepth++;
             return attenuation * trace(scatteredRay);
         }
+        bounceDepth = 0;
         return color (0, 0, 0);
     }
-    return color (0, 0, 0);
-    /*
-    //background
+    bounceDepth = 0;
+    //ambience
     float t = normalized(r.direction).y;  //[-viewportW/2, viewportW/2]
     t += VIEWPORT_HEIGHT/2;              // [0, viewportW]
     t /= VIEWPORT_HEIGHT;               //  [0, 1]
     color result = 
-    color(0.5f, 0.7f, 1.0f)*t + color(1.f, 1.f, 1.f)*(1-t);
-    // return result;
-    return color(0.25f*t, 0.5f*t, t); */
+    color(0.0f, 0.0f, 0.0f)*t + color(1.f, 1.f, 1.f)*(1-t);
+   // return result;
+    return color(1.0f*t, 0.0f*t, 0.0f*t); 
 }
 int main ()
 {
     camera cam;
     color* pixelBuffer = new color[PX_HEIGHT*PX_WIDTH]; //put it on the heap to avoid stack overflow
-    metal grayMetal (color (0.55, 0.78, 1.0), 0.4f);
+    metal grayMetal (color (0.55, 0.78, 1.0), 0.15f);
     metal redMetal (color (1.0, 0.65, 0.65), 0.4f);
     diffuse redDiffuse (color(1.0, 0.25, 0.25));
     dialectric d(1.5f);
     light l(color(1, 1, 1));
     light lAmbient (color(1.0, 0.45, 0.6));
     scene.push_back(new sphere(point(0, 0.25, -1.2), 0.5f, &grayMetal));
-    scene.push_back(new sphere(point(0.5, -20.0, -4.0), 20.f, &grayMetal));
+    scene.push_back(new sphere(point(0.5, -20.0, -4.0), 20.f, &redMetal));
     scene.push_back(new sphere(point(0.0, -0.15, -0.75), 0.1, &redDiffuse));
     scene.push_back(new sphere(point(2.2, 1, -1), 1.5, &l));
     scene.push_back(new sphere(point(-20, 0, -4), 10, &lAmbient));
     for (int i = 0; i < PX_HEIGHT; i++)
     {
-        std::cout << "\rLines remaining : " << PX_HEIGHT-i << ' ';
         for (int j = 0; j < PX_WIDTH; j++)
         {
+            std::cout << "\rLines remaining : " << PX_HEIGHT-i << ' ';
             //sample multiple rays through the same pixel square, then average the results
             color c(0, 0, 0);
             for (int k = 0; k < samplesPerPixel; k++)
